7b6fb5542c9b9001eac2aec060e5aaf135876fb8
dsl2022 <2015rpro@gmail.com>
2023-05-08 14:40:05 -0400

    first commit

---
+++ easy.js
@@ -0,0 +1,46 @@
+// Find Method:
+// Challenge: Find the first even number in an array.
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Starter code - Add your solution here
+
+
+// Sort Method:
+// Challenge: Sort an array of numbers in ascending order.
+
+var unsortedArray = [5, 3, 8, 1, 9, 2, 7, 4, 6];
+
+// Starter code - Add your solution here
+
+
+// Filter Method:
+// Challenge: Filter out all the strings from an array of mixed data types.
+
+var mixedArray = [1, 'apple', 2, 'banana', 3, 'cherry', 4, 'date'];
+
+// Starter code - Add your solution here
+
+
+// Reduce Method:
+// Challenge: Calculate the sum of all numbers in an array.
+
+var numbersArray = [1, 2, 3, 4, 5];
+
+// Starter code - Add your solution here
+
+
+// Map Method:
+// Challenge: Double each number in an array.
+
+var originalArray = [1, 2, 3, 4, 5];
+
+// Starter code - Add your solution here
+
+
+// forEach Method:
+// Challenge: Print all the elements of an array.
+
+var fruits = ['apple', 'banana', 'cherry', 'date'];
+
+// Starter code - Add your solution here
---
+++ medium.js
@@ -0,0 +1,50 @@
+// Find Method:
+// Challenge: Find the index of the first negative number in an array.
+
+var numbers = [5, -2, 8, -7, 3, -4, 1];
+
+// Starter code - Add your solution here
+
+
+// Sort Method:
+// Challenge: Sort an array of objects based on a specific property.
+
+var persons = [
+  { name: 'Alice', age: 25 },
+  { name: 'Bob', age: 30 },
+  { name: 'Charlie', age: 20 }
+];
+
+// Starter code - Add your solution here
+
+
+// Filter Method:
+// Challenge: Filter out all the elements that are greater than a given threshold in an array.
+
+var numbersArray = [10, 5, 25, 8, 15, 30];
+
+// Starter code - Add your solution here
+
+
+// Reduce Method:
+// Challenge: Find the average of all the numbers in an array.
+
+var scores = [85, 90, 95, 80, 75];
+
+// Starter code - Add your solution here
+
+
+// Map Method:
+// Challenge: Capitalize the first letter of each word in an array of strings.
+
+var words = ['apple', 'banana', 'cherry', 'date'];
+
+// Starter code - Add your solution here
+
+
+// forEach Method:
+// Challenge: Multiply each element of an array by its index.
+
+var numbers = [1, 2, 3, 4, 5];
+
+// Starter code - Add your solution here
